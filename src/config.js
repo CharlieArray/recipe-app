c4b5fff8d6bb349f5bb7e89f004bbc93a541827e
Charles Ybarra <charles.ybarra@outlook.com>
2021-05-17 20:20:43 -0500

    production endpoint update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     API_ENDPOINT: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients',
-    SERVER_ENDPOINT: 'https://agile-everglades-18375.herokuapp.com/' ||'http://localhost:8000'
+    SERVER_ENDPOINT: 'https://agile-everglades-18375.herokuapp.com' ||'http://localhost:8000'
 }
 
 export default config;

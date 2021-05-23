15c35efa202b288cdc1e6fbe35ecd0fee27cb955
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2021-05-23 17:52:22 -0500

    updates to smoke tests

--- src/Test/App.test.js
+++ src/Test/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from '../App';
+import GroceryMain from '../MainPage/GroceryMain'
+import GroceryForm from '../MainPage/GroceryForm'
+import { AccountBox } from '../LoginPage/index'
+import Router from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//App/Login Page Smoke Test
+it('App renders without crashing', () => {
+  //creates a DOM element to render the component into
+  const div = document.createElement('div');
+
+  // renders the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(<App />, div);
+
+  // cleanup code
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+//Grocery MainPage Smoke Test
+it('GroceryMain Page renders without crashing', () => {
+  const div = document.createElement('div')
+
+  ReactDOM.render(<GroceryMain/> , div)
+})
+
+//GroceryForm Smoke Test
+it('GroceryForm Page renders without crashing', () => {
+  const div = document.createElement('div')
+
+  ReactDOM.render(<GroceryForm/> , div)
+})
+
+
+
